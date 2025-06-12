8eda8c59e921f2ce40bc3a6fb7be0d73081b785b
root <root@srv850217.hstgr.cloud>
2025-06-12 22:38:29 +0000

    plugin subscription : clean settings

--- local/subscriptions/settings.php
+++ local/subscriptions/settings.php
@@ -1,23 +1,35 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
 
+use local_subscriptions\subscription_config;
+
 if ($hassiteconfig) {
-    $ADMIN->add('localplugins', new admin_externalpage(
-        'local_subscriptions_manage',
-        get_string('active_subscriptions', 'local_subscriptions'),
-        new moodle_url('/local/subscriptions/manage.php')
+
+	// Crée une catégorie principale "Subscriptions"
+	$ADMIN->add('localplugins', new admin_category(
+		'subscriptions_category', 
+		'🧾 ' . get_string('pluginname', 'local_subscriptions')));
+
+	// Ajoute les pages dans la catégorie
+
+    $ADMIN->add('subscriptions_category', new admin_externalpage(
+        'local_subscriptions_manage_subscription',
+        get_string('manage_subscriptions', 'local_subscriptions'),
+        new moodle_url(subscription_config::manage_subscription_page()),
+		'moodle/site:config'
     ));
 
-	$ADMIN->add('localplugins', new admin_externalpage(
-		'local_subscriptions_manualenrol',
-		get_string('manual_enrol', 'local_subscriptions'),
-		new moodle_url('/local/subscriptions/manual_enrol.php')
+	$ADMIN->add('subscriptions_category', new admin_externalpage(
+		'local_subscriptions_add_subscription',
+		get_string('add_subscription', 'local_subscriptions'),
+		new moodle_url(subscription_config::add_subscription_page()),
+		'moodle/site:config'
 	));
 	
-	$ADMIN->add('localplugins', new admin_externalpage(
-		'local_subscriptions_importcsv',
-		get_string('import_csv', 'local_subscriptions'),
-		new moodle_url('/local/subscriptions/import_csv.php'),
+	$ADMIN->add('subscriptions_category', new admin_externalpage(
+		'local_subscriptions_import_csv',
+		get_string('import_subscriptions_csv', 'local_subscriptions'),
+		new moodle_url(subscription_config::import_csv_page()),
 		'moodle/site:config'
 	));
 
