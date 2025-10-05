1b126f21ce1f8fdcff93beb01ac2f2755fbdcd92
root <root@srv850217.hstgr.cloud>
2025-10-05 18:12:27 +0000

    add flexsections plugin

---
+++ course/format/flexsections/classes/courseformat/stateupdates.php
@@ -0,0 +1,36 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace format_flexsections\courseformat;
+
+/**
+ * class stateupdates
+ *
+ * @package   format_flexsections
+ * @copyright 2022 Marina Glancy
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class stateupdates extends \core_courseformat\stateupdates {
+
+    /**
+     * Add track about a course module removed.
+     *
+     * @param int $cmid the affected course module id
+     */
+    public function add_cm_remove(int $cmid): void {
+        $this->add_update('cm', 'remove', (object)['id' => $cmid]);
+    }
+}
