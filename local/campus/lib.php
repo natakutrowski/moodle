8aadd48e5e856568dee8f9b7ce03c9365061dac5
root <root@srv850217.hstgr.cloud>
2026-04-24 15:29:00 +0000

    re-enable discount banner with countdown 26/04/26

--- local/campus/lib.php
+++ local/campus/lib.php
@@ -270,7 +270,9 @@ function local_campus_render_trial_discount_banner(?bool $showcta = null): void 
     }
 
     $discPct   = (int)(get_config('local_subscriptions','trial_discount_percent') ?? 15);
-    $deadline  = (int)\local_subscriptions\trial_manager::discount_window_deadline((int)$USER->id);
+    $dt = new DateTime('2026-04-26 00:00:00', new DateTimeZone('Europe/Paris'));
+    $deadline  = (int)$dt->getTimestamp();
+    //$deadline  = (int)\local_subscriptions\trial_manager::discount_window_deadline((int)$USER->id);
     $subscribe = (new moodle_url('/local/subscriptions/subscribe.php'))->out(false);
 
     // Détection auto : pas de CTA sur subscribe.php
@@ -361,9 +363,9 @@ function local_campus_get_trial_discount_banner_html(): string {
     }
 
     // disable banner
-/*     ob_start();
+    ob_start();
     local_campus_render_trial_discount_banner();
-    return trim(ob_get_clean()); */
+    return trim(ob_get_clean());
 
     return '';
 }
